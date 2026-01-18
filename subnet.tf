3ebdc2bec355b46529b1eeafc9180a572bc7fe2d
erui123 <93332882+erui123@users.noreply.github.com>
2026-01-19 02:46:26 +0900

    Add public subnet and private subnet

---
+++ subnet.tf
@@ -0,0 +1,22 @@
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-subnet"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "dev-private-subnet"
+  }
+}
